6bee88632d7db30588e57cba3d32076c5858ee3e
Gustavo Martínez <tavo962@gmail.com>
2016-09-17 19:43:25 -0500

    MySQL

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -6,56 +6,7 @@
 # ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
 #  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
 
-# Basic
-rm -rf {file}
-chmod -R 755 {directory}
-chown -R {user}:{user} {directory}
+# MySQL
+mysqldump --user {user} --password --default-character-set=utf8 --events --routines {database} > {file}.sql
 
-# Download file
-curl {URL} > {output}
-
-# Compress
-zip -r {file}.zip {path}
-unzip {file}.zip
-
-# Remove directory
-rm -rf directory
-
-# Create user
-
-# Hostname
-# --------------------------------------------------------------
-# Change
-echo "codeman" >> /etc/hosts
-hostname {codeman}
-
-# Show
-hostname
-# --------------------------------------------------------------
-
-# Show
-hostname
-
-# Last command
-npm install -g sass
-sudo !!
-
-# Write to file
-# --------------------------------------------------------------
-# Overwrite
-echo "test" > file
-
-# Push
-echo "test" >> file
-# --------------------------------------------------------------
-
-# MySQL remote
-# --------------------------------------------------------------
-# Add in my.cnf
-skip-external-locking
-bind-address	=	{ip}|0.0.0.0
-
-# Add in console MySQL
-GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY 'password';
-FLUSH PRIVILEGES;
-# --------------------------------------------------------------
+# TODO: hacer que tome el nombre de la base de datos y lo respalde en un path especifico por el usuario. El archivo se debe de crear name_date.sql
