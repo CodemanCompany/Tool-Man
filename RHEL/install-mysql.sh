62e2dc3561da507c2faf11bce271615d182af7a2
Gustavo Martínez <gustavo@codeman.company>
2016-11-26 15:44:34 -0600

    RHEL

---
+++ RHEL/install-mysql.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#  ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+# ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+# ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+# ██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+# ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+#  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+# Variables
+package="mysql57-community-release-el7-9.noarch.rpm"
+
+clear
+echo "\n ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝"
+echo "\nInstall MySQL"
+
+wget http://dev.mysql.com/get/$package --output-document=/root/$package
+yum localinstall /root/$package
+yum repolist enabled | grep "mysql.*-community.*"
+yum update
+yum install mysql-community-server
+rm -rf /root/$package
+clear
+service mysqld start
+service mysqld status
+
+# mysql_secure_installation
