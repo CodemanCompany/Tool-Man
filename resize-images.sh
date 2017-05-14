f93fc9a131b25b559dafa6ab9450504706065313
Codeman Company <gustavo@codeman.company>
2017-05-13 19:09:14 -0500

    Resize Images

--- resize-images.sh
+++ resize-images.sh
@@ -15,7 +15,7 @@ width="1024"
 for image in ~/Desktop/images/*.jpg;
 do
 	echo "$image"" <=> ""${image%.*}$now.$extension"
-	convert "$image" -quality 100 -resize $width "${image%.*}$now.$extension"
+	convert "$image" -quality 95 -resize $width "${image%.*}$now.$extension"
 
 	# Optional
 	rm "$image"
