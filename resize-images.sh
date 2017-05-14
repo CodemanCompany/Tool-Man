9911adeede2cca0f6e739317aa7657385ee6eb11
Codeman Company <gustavo@codeman.company>
2017-05-13 19:06:57 -0500

    Resize Images

---
+++ resize-images.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#  ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+# ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+# ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+# ██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+# ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+#  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+# apt-get install imagemagick
+
+extension="jpg"
+now="IMAGEMAGICK"
+width="1024"
+
+for image in ~/Desktop/images/*.jpg;
+do
+	echo "$image"" <=> ""${image%.*}$now.$extension"
+	convert "$image" -quality 100 -resize $width "${image%.*}$now.$extension"
+
+	# Optional
+	rm "$image"
+done;
+
+# Cache
+rm -rf /tmp/magick-*
