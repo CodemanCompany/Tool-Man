47e1d9135aa03fdac0e2c110e70fad66cf45772c
Gustavo Martínez <tavo962@gmail.com>
2018-02-26 12:47:39 -0600

    Padding image

---
+++ padding-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#  ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+# ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+# ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+# ██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+# ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+#  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+# apt-get install imagemagick
+
+extension="png"
+now="IMAGEMAGICK"
+width="200"
+height="200"
+
+for image in ~/Desktop/images/*.png;
+do
+	echo "$image <=> ${image%.*}$now.$extension"
+	convert \
+	"$image" \
+	-background none \
+	-gravity center  \
+	-extent 200x200 \
+	"${image%.*}$now.$extension"
+
+	# Optional
+	# rm "$image"
+done;
+
+# Cache
+rm -rf /tmp/magick-*
