b8eff1587b23705dfeb19d8c16d1758e4b68d3a0
Gustavo Martínez <gustavo@codeman.company>
2018-09-17 15:21:19 -0500

    Progress Linux

--- resize-images.sh
+++ resize-images.sh
@@ -9,13 +9,18 @@
 # apt-get install imagemagick
 
 extension="jpg"
+# extension="png"
 now="IMAGEMAGICK"
-width="1024"
+width="350"
 
 for image in ~/Desktop/images/*.jpg;
+# for image in ~/Desktop/images/*.png;
 do
 	echo "$image <=> ${image%.*}$now.$extension"
+	# Crop => -trim
 	convert "$image" -quality 95 -resize $width "${image%.*}$now.$extension"
+	
+	# convert "$image" -gravity center -background transparent -extent 330x330 "${image%.*}$now.$extension"
 
 	# Optional
 	# rm "$image"
