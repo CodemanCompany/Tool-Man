eb44d7f899c18687c1f1dd9c81fe465d6c5828ff
Erick Alva <erick@Mac-mini-de-Erick.local>
2018-09-21 20:27:57 -0500

    feat: resizes to a specific size

--- resize-images.sh
+++ resize-images.sh
@@ -20,7 +20,10 @@ do
 	# Crop => -trim
 	convert "$image" -quality 95 -resize $width "${image%.*}$now.$extension"
 	
-	# convert "$image" -gravity center -background transparent -extent 330x330 "${image%.*}$now.$extension"
+	#convert "$image" -gravity center -background transparent -extent 330x330 "${image%.*}$now.$extension"
+	
+	#  This command resizes to a specific size
+	#convert "$image" -gravity center -extent 1100x700 "${image%.*}$now.$extension"
 
 	# Optional
 	# rm "$image"
